44664c67c849ef5ba630cf7d46aa77f597440302
Antal Áron <antalaron@antalaron.hu>
2017-03-03 13:33:08 +0100

    Added controller, exception

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * (c) Antal Áron <antalaron@antalaron.hu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Antalaron\Bundle\DemoBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * DefaultController.
+ *
+ * @author Antal Áron <antalaron@antalaron.hu>
+ */
+class DefaultController
+{
+    public function indexAction()
+    {
+        return new Response('Ok');
+    }
+}
---
+++ src/Exception/DemoExceptionInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * (c) Antal Áron <antalaron@antalaron.hu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Antalaron\Bundle\DemoBundle\Exception;
+
+/**
+ * DemoExceptionInterface.
+ *
+ * @author Antal Áron <antalaron@antalaron.hu>
+ */
+interface DemoExceptionInterface
+{
+}
